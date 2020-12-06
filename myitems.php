6b836f49da3e29df55aabc5679dcb2e480431b6d
terrainbuilder52 <32080764+terrainbuilder52@users.noreply.github.com>
2020-12-06 12:09:27 -0600

    changed everything to php

--- myitems.php
+++ myitems.php
@@ -1,5 +1,7 @@
-<head>
-<title>Djinn and Tonic</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Djinn and Tonic</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -7,9 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" href="index.php">Home</a>
@@ -23,10 +25,27 @@
             <li class="nav-item">
               <a class="nav-link" href="profile.php">Profile</a>
             </li>
+            <li>
+            </li>
           </ul>
         </nav>
 
-		<?php
+     <div class="jumbotron">
+       <h1 class="display-3">Djinn and Tonic</h1>
+       <p class="lead">For all your potion needs</p>
+       <hr class="my-2">
+       <form class="form-inline" action="dbconnect.php">
+        <input class="form-control mr-sm-2" type="text" placeholder="Item Name" name="itemName">
+        <input class="form-control mr-sm-2" type="text" placeholder="Item Price" name="itemPrice">
+        <input class="form-control mr-sm-2" type="radio" placeholder="Potion" name="itemPotion">
+        <label for="radio">Potion</label>
+        <input class="form-control mr-sm-2" type="radio" placeholder="Ingredient" name="itemIngredient">
+        <label for="radio">Ingredient</label>
+        <button class="btn btn-success" type="submit">Sell</button>
+      </form>
+     </div>
+
+     <?php
 	      // Make a connection to the database
         // The values here MUST BE CHANGED to match the database and credentials you wish to use
         //$dbhost = pg_connect("host=hostname dbname=databasename user=username password=password");
@@ -71,5 +90,12 @@
         pg_close($dbhost);
       ?>
 
-</body>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
